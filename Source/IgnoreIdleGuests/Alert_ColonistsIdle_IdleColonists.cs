725acb881fc30aade066eee5dbe550b5846b150b
emipa606 <emil.offermo@litet.net>
2021-06-12 21:27:41 +0200

    First publish

---
+++ Source/IgnoreIdleGuests/Alert_ColonistsIdle_IdleColonists.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace IgnoreIdleGuests
+{
+    [HarmonyPatch(typeof(Alert_ColonistsIdle), "IdleColonists", MethodType.Getter)]
+    public class Alert_ColonistsIdle_IdleColonists
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref List<Pawn> __result)
+        {
+            if (!__result.Any())
+            {
+                return;
+            }
+
+            var ignorePawns = __result.Where(pawn => pawn.WorkTagIsDisabled(WorkTags.AllWork)).ToList();
+
+            __result.RemoveAll(pawn => ignorePawns.Contains(pawn));
+        }
+    }
+}
---
+++ Source/IgnoreIdleGuests/IgnoreIdleGuests.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace IgnoreIdleGuests
+{
+    [StaticConstructorOnStartup]
+    public static class IgnoreIdleGuests
+    {
+        static IgnoreIdleGuests()
+        {
+            var harmony = new Harmony("Mlie.IgnoreIdleGuests");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
